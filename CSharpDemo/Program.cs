d6bdda494ebb148dffa69f7e4e6a5d644fae3d31
balalawy <47027345+balalawy@users.noreply.github.com>
2020-05-15 15:29:36 +0800

    学习了C#中的委托Delegate的使用

--- CSharpDemo/Program.cs
+++ CSharpDemo/Program.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 
-
+// https://www.cnblogs.com/SkySoot/archive/2012/04/05/2433639.html
+// C#中的委托和事件详解
 namespace CSharpDemo
 {
+    // 定义委托
+    public delegate void GreetingDelegate(string name);
+
     class Program
     {
+        public static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Good Morning, " + name);
+        }
+
+
+        public static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好, " + name);
+        }
+
+
+        // 调用了委托GreetingDelegate
+        public static void GreetPeople(string name, GreetingDelegate makeGreeting)
+        {
+            makeGreeting(name);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 使用委托方式一：
+            GreetPeople("Mike", EnglishGreeting);
+            GreetPeople("张三", ChineseGreeting);
+
+            // 使用委托方式二：
+            // 可以将多个方法赋给同一个委托，或者叫将多个方法绑定到同一个委托，
+            // 当调用这个委托的时候，将依次调用其所绑定的方法。
+            GreetingDelegate delegateFunc;
+            delegateFunc = EnglishGreeting;
+            delegateFunc += ChineseGreeting;
+            GreetPeople("Jake", delegateFunc);
         }
     }
 }
