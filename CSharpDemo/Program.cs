a0ff21806b23e0b7e6206d36252951f55e27a7df
balalawy <47027345+balalawy@users.noreply.github.com>
2020-05-15 15:03:18 +0800

    初始化项目

---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
